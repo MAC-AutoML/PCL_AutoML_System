d08ec015a3e87ed010f74e3a3052738f56b28ad2
zhengxiawu <zhengxiawu@126.com>
2020-10-04 20:23:23 +0800

    add tables

--- AutoML_Web/_app/admin.py
+++ AutoML_Web/_app/admin.py
@@ -5,3 +5,7 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.User)
+admin.site.register(models.Dataset)
+admin.site.register(models.Algorithm)
+admin.site.register(models.User_algorithm)
+admin.site.register(models.User_Job)
--- AutoML_Web/_app/models.py
+++ AutoML_Web/_app/models.py
@@ -4,27 +4,50 @@ from django.db import models
 
 
 class User(models.Model):
-
-    gender = (
-        ('male', "男"),
-        ('female', "女"),
-    )
-
     name = models.CharField(max_length=128, unique=True)
     password = models.CharField(max_length=256)
     email = models.EmailField(unique=True)
-    sex = models.CharField(max_length=32, choices=gender, default="男")
     c_time = models.DateTimeField(auto_now_add=True)
+    _path = models.CharField(max_length=256, default='')
 
     def __str__(self):
         return self.name
 
     class Meta:
         ordering = ["-c_time"]
-        verbose_name = "用户"
-        verbose_name_plural = "用户"
 
 
+# public dataset class
 class Dataset(models.Model):
     name = models.CharField(max_length=128, unique=True)
     task = models.CharField(max_length=128)
+    _path = models.CharField(max_length=256, default='')
+    is_check = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-id"]
+
+
+# public algorithm class
+class Algorithm(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    task = models.CharField(max_length=128)
+    # code path
+    _path = models.CharField(max_length=256, default='')
+    is_check = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-id"]
+
+
+class User_algorithm(models.Model):
+    name = models.CharField(max_length=128)
+    user_id = models.CharField(max_length=128, default='')
+    _path = models.CharField(max_length=256, default='')
+
+
+class User_Job(models.Model):
+    job_name = models.CharField(max_length=128)
+    task = models.CharField(max_length=128)
+    dataset_name = models.CharField(max_length=128)
+    _path = models.CharField(max_length=256, default='')
