b9de74dfd1e58544adc9aaca2d4e9dd169c7e683
zhengxiawu <zhengxiawu@126.com>
2020-10-04 17:22:24 +0800

    login page

--- AutoML_Web/AutoML_Web/urls.py
+++ AutoML_Web/AutoML_Web/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from login import login_views
+from login import views as login_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- AutoML_Web/login/views.py
+++ AutoML_Web/login/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 # Create your views here.
+
+
+def index(request):
+    pass
+    return render(request, 'index.html')
+
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
+        return redirect('/index/')
+    return render(request, 'login.html')
+
+
+def register(request):
+    pass
+    return render(request, 'register.html')
+
+
+def logout(request):
+    pass
+    return redirect("/login/")
