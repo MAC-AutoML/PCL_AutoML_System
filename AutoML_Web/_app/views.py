393df62ea6600bea19292418dc2578c56bbd19a9
zhengxiawu <zhengxiawu@126.com>
2020-10-09 15:22:48 +0800

    formatting

--- AutoML_Web/_app/views.py
+++ AutoML_Web/_app/views.py
@@ -11,21 +11,23 @@ def index(request):
     pass
     return render(request, 'index.html')
 
+
 def login(request):
     if(request.method == "GET"):
-        return render(request,"login.html")
+        return render(request, "login.html")
     elif request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
         # valid_num = request.POST.get("valid_num")
         # keep_str = request.session.get("keep_str")
         message = '请检查填写的内容！'
-        user=auth.authenticate(username=username,password=password) # 验证是否存在用户
+        user = auth.authenticate(
+            username=username, password=password)  # 验证是否存在用户
         if(user):
-            auth.login(request,user)
+            auth.login(request, user)
             return redirect('/userinfo/')
         else:
-            message="用户名或密码错误！"
+            message = "用户名或密码错误！"
             return render(request, 'login.html', {'message': message})
     return render(request, 'login.html')
 
@@ -34,50 +36,54 @@ def register(request):
     if(request.method == "GET"):
         return render(request, 'register.html')
     elif(request.method == "POST"):
-        username = request.POST.get('username','')
-        password = request.POST.get('password','') 
-        repeat_password = request.POST.get('repeat_password', '') 
-        email=request.POST.get('email', '')   
-        if( models.User.objects.filter(username=username) or username == '0'):
-            content={
-                'username' :username
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        repeat_password = request.POST.get('repeat_password', '')
+        email = request.POST.get('email', '')
+        if(models.User.objects.filter(username=username) or username == '0'):
+            content = {
+                'username': username
             }
-            return render(request,'register.html',content)
+            return render(request, 'register.html', content)
         elif(password == repeat_password):
-            new_user=models.User.objects.create_user(username=username,
-            password=password,email=email)
+            new_user = models.User.objects.create_user(username=username,
+                                                       password=password, email=email)
             new_user.save()
             return redirect('/index/')
         pass
+
+
 @login_required
 def set_password(request):
-    iuser=request.user
-    state=None
+    iuser = request.user
+    state = None
     if(request.method == "GET"):
         return render(request, 'set_password.html')
-    if(request.method == 'POST' ):
-        old_p=request.POST.get('old_password','')
-        new_p=request.POST.get('new_password','')
-        rep_p=request.POST.get('repeat_password','')
+    if(request.method == 'POST'):
+        old_p = request.POST.get('old_password', '')
+        new_p = request.POST.get('new_password', '')
+        rep_p = request.POST.get('repeat_password', '')
         if(iuser.check_password(old_p)):
             if(not new_p):
-                state='empty'
+                state = 'empty'
             elif(new_p != rep_p):
-                state='not the same password'
+                state = 'not the same password'
             else:
                 iuser.set_password(new_p)
                 iuser.save()
                 return redirect('/userinfo/')
-        content={
-            'user' :iuser,
-            'state' : state
+        content = {
+            'user': iuser,
+            'state': state
         }
-        return render(request,'set_password.html',content)
+        return render(request, 'set_password.html', content)
+
 
 def logout(request):
     auth.logout(request)
     return redirect("/login/")
 
+
 def userinfo(request):
-    
-    return render(request,"userinfo.html")
+
+    return render(request, "userinfo.html")
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # PCL_AutoML_System
+
 This project is based on Django (Backend), Bootstrap/Vue.js (Frontend)
 
 ## Environment:
 
-- Python 3.8
-- Django 2.2
+* Python 3.8
+* Django 2.2
