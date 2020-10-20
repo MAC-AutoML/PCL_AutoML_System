939a81ec708dbeeed4a66c17863a23189db71cbe
dercaft <wuyuhang2073@gmail.com>
2020-10-20 15:32:37 +0800

    Shift to adminlte3

--- AutoML_Web/AutoML_Web/settings.py
+++ AutoML_Web/AutoML_Web/settings.py
@@ -130,6 +130,5 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
+STATIC_ROOT = os.path.join(str(BASE_DIR), "static")
 STATIC_URL = '/static/'
