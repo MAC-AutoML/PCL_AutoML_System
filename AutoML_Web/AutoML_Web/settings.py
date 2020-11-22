325764ec83ec69df3528f2b8b4e44835eb06f264
dercraft <wuyuhang2073@gmail.com>
2020-11-22 20:24:20 +0800

    ATTENTION: Change database to .sqllite3

--- AutoML_Web/AutoML_Web/settings.py
+++ AutoML_Web/AutoML_Web/settings.py
@@ -78,19 +78,19 @@ WSGI_APPLICATION = 'AutoML_Web.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'automl_web',
-        # 'NAME': 'wuyuhang',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'automl_web',
+    #     # 'NAME': 'wuyuhang',
+    #     'USER': 'root',
+    #     'PASSWORD': 'root',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '3306',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
