c31d4529c9516f69247ffe09ef06e04969cd6a50
johsnows <sdut_snow@163.com>
2020-12-07 21:57:57 +0800

    update bbo

--- BBO/bayesmark/bayesmark/cmd_parse.py
+++ BBO/bayesmark/bayesmark/cmd_parse.py
@@ -244,7 +244,7 @@ def launcher_parser(description):
 
 
 def experiment_parser(description):
-    parser = argparse.ArgumentParser(description=description, parents=[base_parser()])
+    parser = argparse.ArgumentParser(description=description)
 
     add_argument(parser, CmdArgs.uuid, type=uuid, required=True, help="length 32 hex UUID for this experiment")
 
