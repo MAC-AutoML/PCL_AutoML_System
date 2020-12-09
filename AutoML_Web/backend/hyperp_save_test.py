1ffeb19814d8e6b44c91f673ade9df9e866f2ce4
woodchen7 <1004463561@qq.com>
2020-12-09 11:26:28 +0800

    超参保存测试

---
+++ AutoML_Web/backend/hyperp_save_test.py
@@ -0,0 +1,15 @@
+import yaml
+
+d = {'a':1, 0:2, 'sd':{0:1,2:{3:1}}}
+fp = open('../HyperP_Save/dict_debug.yaml', 'w')
+fp.write(yaml.dump(d))
+fp.close()
+
+import yaml
+
+fp = open('../HyperP_Save/dict_debug.yaml', 'r')
+st = fp.read()
+fp.close()
+
+dd = yaml.load(st,Loader=yaml.FullLoader)
+print(dd)
